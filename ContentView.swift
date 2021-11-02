f76bb62a932a7f8e0abbb6e42580abb45dbf7765
진시윤 <z01039118740@gmail.com>
2021-11-02 19:52:05 +0900

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial
+//
+//  Created by GSM03 on 2021/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ㅋㅋㅋㅋㅋㅋ!")
+            .fontWeight(.medium)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorialApp.swift
+//  SwiftUI_tutorial
+//
+//  Created by GSM03 on 2021/11/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
