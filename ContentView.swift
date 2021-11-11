6753f71f6c389569a515ddae79aaf52b376fad0b
진시윤 <z01039118740@gmail.com>
2021-11-11 09:59:15 +0900

    .

--- ContentView.swift
+++ ContentView.swift
@@ -50,6 +50,7 @@ struct ContentView: View {
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+
             
         }
     }
